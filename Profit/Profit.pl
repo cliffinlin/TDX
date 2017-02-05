ef80d999347fe687b791cbf123f034176ab6b418
cliffinlin <cliffinlin@163.com>
2017-02-05 16:30:04 +0800

    update, add TRUE, FALSE variables

--- Profit/Profit.pl
+++ Profit/Profit.pl
@@ -18,9 +18,14 @@ print("Generate $FileName\n");
 tie @LineArray, 'Tie::File', $FileName or die "Unable tie array to $FileName";
 @LineArray = ();
 
+push(@LineArray, "TRUE:=1;");
+push(@LineArray, "FALSE:=0;");
+push(@LineArray, "\r");
+
 push(@LineArray, "MONEY:=10000;");
 push(@LineArray, "\r");
-push(@LineArray, "TRANSACTION_FEE:=2/1000;");
+
+push(@LineArray, "FEE:=2/1000;");
 push(@LineArray, "\r");
 
 $i = 0;
@@ -38,17 +43,18 @@ foreach my $Period (@MAPeriod)
     push(@LineArray, "HOLD_$i:=EXP_MA_$i>REF(EXP_MA_$i,1);");
     push(@LineArray, "\r");
     
-    push(@LineArray, "RATIO_$i:=IF(HOLD_$i=1,PERCENT_$i,0);");
+    push(@LineArray, "RATIO_$i:=IF(HOLD_$i=TRUE,PERCENT_$i,0);");
+    push(@LineArray, "MONEY_BUY_$i:=MONEY*RATIO_$i*(1-FEE);");
     push(@LineArray, "\r");
     
-    push(@LineArray, "PRICE_$i:=REF(C,BARSLAST(HOLD_$i<>1)-1);");
-    push(@LineArray, "VOLUME_$i:=INTPART(MONEY*RATIO_$i*(1-TRANSACTION_FEE)/PRICE_$i/100)*100;");
+    push(@LineArray, "PRICE_$i:=REF(C,BARSLAST(HOLD_$i=FALSE)-1);");
+    push(@LineArray, "VOLUME_$i:=INTPART(MONEY_BUY_$i/PRICE_$i/100)*100;");
     push(@LineArray, "VALUE_$i:=VOLUME_$i*C;");
     push(@LineArray, "\r");
     
-    push(@LineArray, "BUY_VOLUME_$i:=IF(BUY_$i=1,VOLUME_$i,0);");
-    push(@LineArray, "SELL_VOLUME_$i:=IF(SELL_$i=1,REF(VOLUME_$i,1),0);");
-    push(@LineArray, "PROFIT_VOLUME_$i:=VOLUME_$i-BUY_VOLUME_$i; {+SELL_VOLUME_$i;}");    
+    push(@LineArray, "BUY_VOLUME_$i:=IF(BUY_$i=TRUE,VOLUME_$i,0);");
+    push(@LineArray, "SELL_VOLUME_$i:=IF(SELL_$i=TRUE,REF(VOLUME_$i,1),0);");
+    push(@LineArray, "PROFIT_VOLUME_$i:=VOLUME_$i-BUY_VOLUME_$i; {+SELL_VOLUME_$i;}");
     push(@LineArray, "\r");
 
     push(@LineArray, "PROFIT_$i:=PROFIT_VOLUME_$i*(C-PRICE_$i)*100/MONEY;");
@@ -58,15 +64,15 @@ foreach my $Period (@MAPeriod)
     push(@LineArray, "RATIO$i:RATIO_$i;");
     push(@LineArray, "PROFIT$i:PROFIT_VOLUME_$i*(C-PRICE_$i)*100/MONEY;");
     push(@LineArray, "SUM_PROFIT$i:SUM_PROFIT_$i/MONEY;");
-    push(@LineArray, "{---------------------------------------------------------------------------------------}");
+    push(@LineArray, "{---------------------------------------------------------------------------------------};");
     push(@LineArray, "\r");
     
     if ($i == 0) {
         $TOTAL_RATIO .= "RATIO_$i";
-        $TOTAL_PROFIT .= "SUM_PROFIT_$i"; 
+        $TOTAL_PROFIT .= "SUM_PROFIT_$i";
     } else {
         $TOTAL_RATIO .= "+RATIO_$i";
-        $TOTAL_PROFIT .= "+SUM_PROFIT_$i";   
+        $TOTAL_PROFIT .= "+SUM_PROFIT_$i";
     }
 
     $i++;
